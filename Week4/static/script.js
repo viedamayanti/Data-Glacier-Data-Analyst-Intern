2b4f96b6a4f06471e9ca1562c781154480128552
viedamayanti <vinaniekdamayanti@gmail.com>
2024-01-15 15:02:47 +0100

    week 4 update

--- Week4/static/script.js
+++ Week4/static/script.js
@@ -1,14 +1,22 @@
 document.getElementById('submitBtn').addEventListener('click', function () {
+  const pregnancies = document.getElementById('pregnancies').value;
   const glucose = document.getElementById('glucose').value;
   const insulin = document.getElementById('insulin').value;
+  const bloodPressure = document.getElementById('blood_pressure').value;
+  const skinThickness = document.getElementById('skin_thickness').value;
   const bmi = document.getElementById('bmi').value;
   const age = document.getElementById('age').value;
+  const diabetesPedigree = document.getElementById('diabetesPedigree').value;
 
   const formData = {
+    pregnancies,
     glucose,
     insulin,
+    bloodPressure,
+    skinThickness,
     bmi,
     age,
+    diabetesPedigree,
   };
 
   console.log(formData);
